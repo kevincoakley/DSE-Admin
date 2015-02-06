2c274d938a8d4174afcf175fc0ed765802aa3581
Kevin Coakley <kcoakley@sdsc.edu>
2015-02-06 15:10:06 -0800

    Update the DSE200 and DSE200-notebooks repositories at boot

--- AMI/install.sh
+++ AMI/install.sh
@@ -38,7 +38,7 @@ source /home/ubuntu/.bashrc
 /home/ubuntu/anaconda/bin/conda install pysam --yes
 /home/ubuntu/anaconda/bin/conda install pyside --yes
 /home/ubuntu/anaconda/bin/conda install pyzmq --yes
-/home/ubuntu/anaconda/bin/conda install imaging —yes
+/home/ubuntu/anaconda/bin/conda install imaging —-yes
 /home/ubuntu/anaconda/bin/conda install paramiko --yes
 
 # Install PIP Packages
@@ -70,3 +70,23 @@ cd ~
 /bin/ln -s ~/DSE200/data/ ~/data
 /bin/ln -s ~/DSE200/AWS_scripts/ ~/scripts
 /bin/ln -s ~/DSE200-notebooks/ ~/notebooks
+
+# Update the DSE200 and DSE200-notebooks repositories at boot
+/bin/cat <<EOF | /usr/bin/sudo /usr/bin/tee /etc/rc.local
+#!/bin/sh -e
+#
+# rc.local
+#
+# This script is executed at the end of each multiuser runlevel.
+# Make sure that the script will "exit 0" on success or any other
+# value on error.
+#
+# In order to enable or disable this script just change the execution
+# bits.
+#
+# By default this script does nothing.
+su ubuntu -c "cd /home/ubuntu/DSE200; git pull"
+su ubuntu -c "cd /home/ubuntu/DSE200-notebooks; git pull"
+
+exit 0
+EOF
